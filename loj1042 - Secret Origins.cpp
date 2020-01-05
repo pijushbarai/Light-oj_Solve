ab3b14095e020144dfcd78c4af074d93e02f9228
Pijush Barai <pijushbarai1@gmail.com>
2020-01-06 00:38:32 +0600

    This is my another solution of this problem but the judge says that this is wrong answergit add .git add .

--- loj1042 - Secret Origins.cpp
+++ loj1042 - Secret Origins.cpp
@@ -6,58 +6,31 @@ using namespace std;
 int main()
 {
 	int test, t = 0;
-	ll n, x = -2, y = -2;
-	cin >> test;
+	ll n;
+	scanf("%d",&test);
 	while(test--){
 		t++;
-		cin >> n;
-		int no1 = 0;
-		ll l = ceil(log2(n)); 
-		
-		//===finding the place x and y ====
-		for(int i = 1; i < l; i++){
-			//cout << "x = " << x << " y = " << y << endl;
-			if((n &(1 << i)) == 0 && x < 0 && (n &(1 << i-1)) ){
-				x = i;
-				y = i-1;
+		scanf("%lld",&n);
+		for(int i = 0; ;i++){
+			if((n& (1 << i)) && !(n&(1 << i+1))){
+				n = n^(1 << i);
+				n = n^(1 << i+1);
+				//cout << n << " ch " << i << endl;
+				for(int j = i-1; j > 0; j--){
+					//cout << "i am in" << endl;
+					if((n& (1 << j)) && !(n&(1 << j-1))){
+						//cout << n << " ch2" << endl;
+						n = n^(1 << j);
+						n = n^(1 << j-1);
+					}
+				}
 				break;
 			}
 		}
-		
-		
-		//=====findind===value==in various====condition====
-		cout<< endl << endl << "final x = " << x << " final y = " << y << endl;
-		if(l == 0){
-			n =(n^(1 << 1));
-			n =(n^(1 << 0));
-			cout << "Case " << t << ": " << n << endl;
-		}else if(x == 0 && y < 0){
-			n = (1 << l)| (1 << 0);
-			cout << "Case " << t << ": " << n << endl;
-		}
-		else if(x >0 && y >= 0 && (x-y) < 2){
-			n = (n ^(1 << x));
-			n = (n^(1 << y));
-			cout << "Case " << t << ": " << n << endl;
-		}else if(x < 0 && y < 0){
-			n = (n^(1 << l));
-			n = (n^(1 << l-1));
-			cout << "Case " << t << ": " << n << endl;
-		}else if(x > 0 && y < 0){
-			//cout << "l = "<< l << endl;
-			//cout << n << " before " << endl;
-			n = (n^(1 << l+1));
-			//cout << n << " middle " << endl;
-			n = (n^(1 << l));
-			//cout << n << " end " << endl;
-			cout << "Case " << t << ": " << n << endl;
-		}else if(x > 0 && y > 0 && (x - y) > 1){
-			n = (n^(1 << y+1));
-			n = (n^(1 << y));
-			cout << "Case " << t << ": " << n << endl;
-		}
-		x = -2;
-		y = -2;
+		//cout <<"Case " << t << ": "<<  n << endl;
+		printf("Case %d: %lld\n", t, n);
+	//	cout << "Case " << t << ": " << n << endl;
+	
 	}
 	return  0;
 }
